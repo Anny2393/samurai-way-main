4f1bff836312bb2bc36ac9699b59bb2c585981a9
Anna Narushevich <annyit23@gmail.com>
2023-02-21 23:42:05 +0300

    14 lesson

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import ss from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={ss.header}>
         <img src='https://i.pinimg.com/originals/e7/eb/b9/e7ebb9c19633adddc0f06a7869f9c35b.png'/>
     </header>
 }
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import stile from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={stile.content}>
         <div>
             <img src='https://media.istockphoto.com/id/1183534384/sv/vektor/landskap-av-dimmiga-berg-och-barrskog-akvarell-illustration.jpg?s=612x612&w=0&k=20&c=msXh4VX7eD4zgal9LfG4DX5P-ANQcrzT1FF7gypiQcA='/>
         </div>
@@ -13,11 +14,11 @@ const Profile = () => {
             <div>
                 New post
             </div>
-            <div>
-                <div>
+            <div className={stile.posts}>
+                <div className={stile.item}>
                     post 1
                 </div>
-                <div>
+                <div className={stile.item}>
                     post 2
                 </div>
             </div>
